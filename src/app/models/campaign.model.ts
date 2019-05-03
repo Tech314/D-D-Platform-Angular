66cf1100ef68b139a0df3f66e980b4edbd43a41b
Jeffafa1992 <nicholasrocksvold@yahoo.com>
2019-05-03 16:42:09 -0400

    added campaign model

---
+++ src/app/models/campaign.model.ts
@@ -0,0 +1,25 @@
+export class Characters { 
+    campId: number;
+    campName: string;
+    campPass: string;
+    char1: number;
+    char2: number;
+    char3: number;
+    char4: number;
+    char5: number;
+    char6: number;
+
+    constructor(campId: number, campName: string, campPass: string, char1: number,
+        char2: number,char3: number,char4: number,char5: number,char6: number){
+            this.campId = campId;
+            this.campName = campName;
+            this.campPass = campPass;
+            this.char1 = char1;
+            this.char2 = char2;
+            this.char3 = char3;
+            this.char4 = char4;
+            this.char5 = char5;
+            this.char6 = char6;
+
+    }
+}
--- src/app/services/campaign.service.ts
+++ src/app/services/campaign.service.ts
@@ -1,8 +1,13 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class CampaignService {
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+
+  public login(){
+    
+  }
 
 }
