3adf04602477fe8bae1b0c0ce1f387251f5e52a6
Jeffafa1992 <nicholasrocksvold@yahoo.com>
2019-05-05 19:07:10 -0400

    login progress

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { FormControl, FormGroup } from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -8,11 +9,15 @@ import { Router } from '@angular/router';
 })
 export class LoginComponent implements OnInit {
 
-  
+  myForm: FormGroup;
 
   constructor(private router: Router) { }
 
   ngOnInit() {
+   // this.myForm = this.FormGroup({
+   //   'Email': new FormControl(null, [Validators.required]),
+    //  'Password': new FormControl(null, [Validators.required])
+   //  })
   }
 
   onSubmit(){
