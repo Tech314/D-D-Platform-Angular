5985e79068716f2ba69ac8303143df31e209522e
Tech314 <kyle.magdales@yahoo.com>
2019-05-12 13:47:58 -0400

    fixed prod environment url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,4 +2,4 @@ export const environment = {
   production: true
 };
 
-export const HERO_URL = `http://ec2-18-205-191-213.compute-1.amazonaws.com:8080/DnDPlatform/`;
+export const DND_URL = `http://ec2-18-205-191-213.compute-1.amazonaws.com:8080/DnDPlatform/`;
